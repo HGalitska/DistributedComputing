0b1bc58d7fa1161a9c3c8e36fb5d522ab670ef15
HGalitska <h.galitskaya@gmail.com>
2018-03-23 07:27:37 +0200

    Lab 2.b is done.

---
+++ Thieves/src.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"time"
+	"fmt"
+)
+
+type item struct {
+	name  string
+	price int
+}
+
+func main() {
+	var fromWarehouse = make(chan item)
+	var inTrunk = make(chan item)
+
+	go Ivanov(&fromWarehouse)
+	go Petrov(&fromWarehouse, &inTrunk)
+	go Nechiporchuk(&inTrunk)
+	fmt.Scanln()
+}
+
+func Ivanov(fromWarehouse *chan item) {
+	for i := 0; i < 3; i++ {
+		var treasure = item{"Item", i}
+		log.Println("Stolen ", treasure)
+		*fromWarehouse <- treasure
+	}
+}
+
+func Petrov(fromWarehouse *chan item, inTrunk *chan item) {
+	time.Sleep(1000)
+	for {
+		newItem := <- *fromWarehouse
+		log.Println("Put ", newItem, " in trunk.")
+		*inTrunk <- newItem
+	}
+}
+
+func Nechiporchuk(inTrunk *chan item) {
+	sum := 0
+	for {
+		newItem := <- *inTrunk
+		sum = sum + newItem.price
+		log.Println("Counted ", newItem, " total: ", sum)
+	}
+}
